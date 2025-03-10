87d1fe475800bf4b325edc2c0a7356b154a900e7
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-03-11 00:44:39 +0100

    Cifrado Punto a Punto FULL CHAD DONE

--- CODIGO (SDS-PRAC-CHAD)/Nivel 1 (SDS-PRAC-CHAD)/Nivel 2 (SDS-PRAC-CHAD)/Nivel 3 (SDS-PRAC-CHAD)/Nivel 4 (SDS-PRAC-CHAD)/SDS-PRAC-CHAD (CODIGO)/decryption-tests/decrypt.go
+++ CODIGO (SDS-PRAC-CHAD)/Nivel 1 (SDS-PRAC-CHAD)/Nivel 2 (SDS-PRAC-CHAD)/Nivel 3 (SDS-PRAC-CHAD)/Nivel 4 (SDS-PRAC-CHAD)/SDS-PRAC-CHAD (CODIGO)/decryption-tests/decrypt.go
@@ -9,18 +9,18 @@ import (
 )
 
 func main() {
-	// This is the base64-encoded ciphertext you provided.
-	ciphertextBase64 := "7844boamPzW6V+gAyUlzuMpDJCJN0waXdDOsk9C4zqt3P+g4Bg5/suQ="
+	// Replace this with the base64 ciphertext obtained from the database (or from client update logs)
+	ciphertextBase64 := "wPiMBniZZW+T+7J4TCBvDbxNZpVVlREAEt0dLQW4UFquHtwAe2XI5Jr8Jbcyk8zziVo88pvBY2fV8TGb"
 
-	// Decode the base64 string to get the encrypted data (nonce + ciphertext).
+	// Decode the base64 string to get the encrypted data (nonce + ciphertext)
 	ciphertext, err := base64.StdEncoding.DecodeString(ciphertextBase64)
 	if err != nil {
 		log.Fatalf("Error decoding base64: %v", err)
 	}
 
 	// Replace these with the actual username and password used during encryption.
-	username := "cris2"
-	password := "cris2" // Use the same password provided during encryption.
+	username := "crisCHAD4"
+	password := "crisCHAD4"
 
 	// Derive the encryption key from the password and username.
 	key, err := crypto.DeriveKey(password, username)
